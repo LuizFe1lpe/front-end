53998aee3e3e93e660df23dea88b0ebac66b902d
Anderson815 <anderson_souza33@outlook.com.br>
2023-05-27 18:45:51 -0300

    feat: getAll consumido

--- src/pages/mainpage/MainPage.jsx
+++ src/pages/mainpage/MainPage.jsx
@@ -1,7 +1,35 @@
 import './MainPage.css';
+
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const MainPage = () => {
+
+    const [clientes, setClientes] = useState([]);
+
+    useEffect(() => {
+        console.log(`Estou aqui`);
+        console.log(clientes);
+    })
+
+    useEffect(() => {
+        carregarDados();
+    }, [])
+
+    const carregarDados = async() => {
+        const response = await axios.get(`http://localhost:3001/cliente`)
+            .then(res => {
+                return res.data;
+            })
+            .catch(err => {
+                console.log(`Deu erro ao capturar valores`);
+                console.log(err);
+            });
+
+        setClientes(response);
+    }
+
     return (
         <><div class="container">
             <nav class="MenuLateral">
@@ -24,6 +52,7 @@ const MainPage = () => {
                         +Cadastrar <br/> Novo Cliente
                         </Link>
                     </button>
+                    {/** Fim da navegação */}
 
                 </div>
                 <table>
@@ -34,10 +63,20 @@ const MainPage = () => {
                         <td>e-mail</td>
                     </thead>   
                     <tbody>
-                        <td>Exemplo</td>
-                        <td>(00)00000-0000</td>
-                        <td>123.456.789-00</td>
-                        <td>nome@gmail.com</td>
+                        {   
+                            clientes.map((cliente, indice) => {
+                                if(cliente.nome === "")
+                                    return <></>
+                                return(
+                                    <tr key={indice}>
+                                        <td>{cliente.nome}</td>
+                                        <td>{cliente.telefone}</td>
+                                        <td>{cliente.documento}</td>
+                                        <td>{cliente.email}</td>
+                                    </tr>
+                                )
+                            })
+                        }
                     </tbody>
                 </table>
             </div>
