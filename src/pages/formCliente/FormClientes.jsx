8f6019c0ca2dc069ddfcbb19ba527afbb6701b46
Anderson815 <anderson_souza33@outlook.com.br>
2023-05-27 18:04:13 -0300

    refactor: acerto da arquitetura

---
+++ src/pages/formCliente/FormClientes.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import './FormClientes.css';
+import axios from 'axios';
+
+export default class FormClientes extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state={
+      nome: "",
+      telefone: "",
+      documento:"",
+      email:""
+    }
+  }
+
+  setNome(e){
+    this.setState({
+      nome: e.target.value
+    })
+  }
+
+  setTelefone(e){
+    this.setState({
+      telefone: e.target.value
+    })
+  }
+
+  setCpf(e){
+    this.setState({
+      documento: e.target.value
+    })
+  }
+
+  setEmail(e){
+    this.setState({
+      email: e.target.value
+    })
+  }
+
+  async sendData(){
+
+    await axios.post(`http://localhost:3001/cliente`, this.state)
+      .then(data => {
+        console.log(`Deu Bom aqui`);
+        console.log(data);
+      })
+      .catch(erro => {
+        console.log(this.state);
+        console.log(`Deu ruim aqui`);
+        console.log(erro);
+      });
+  }
+
+  render() {
+    return (
+      <form>
+        <div className="crud_cliente">
+          <div className="crud_column">
+            <label for="nome">Nome</label>
+            <label for="telefone">Telefone</label>
+            <label for="cpf">CPF</label>
+            <label for="email">E-mail</label>
+          </div>
+          <div className="crud_column">
+            <input id="nome" type="text" onChange={(e) => this.setNome(e)} value={this.state.nome}/>
+            <input id="telefone" type="phone" onChange={(e) => this.setTelefone(e)} placeholder="(00)00000-0000"/>
+            <input id="cpf" type="text" onChange={(e) => this.setCpf(e)} placeholder="000.000.000-00"/>
+            <input id="email" type="e-mail" onChange={(e) => this.setEmail(e)} placeholder="exemplo@dominio.com"/>
+          </div>
+        </div>
+            <div className="crud_cliente_buttons">
+              <button type='button' className="btnfinalizar" onClick={() => this.sendData()}>Finalizar Cadastro</button>
+              <button className="btncancelar">Cancelar</button>
+          </div>
+      </form>
+    );
+  }
+}
+
