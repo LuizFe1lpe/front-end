cf84a4a2aee6dd54a693964b44769c6f9b827692
Luiz Felipe <lfelipemacedosantos@hotmail.com>
2023-05-27 16:41:23 -0300

    Terceiro commit

--- src/routes/MainPage.jsx
+++ src/routes/MainPage.jsx
@@ -1,19 +1,28 @@
 import './MainPage.css';
+import FormClientes from '../componentes/form-clientes';
 
 const MainPage = () => {
     return (
-        <nav class="MenuLateral">
-            <div class="login-coluna-esquerda">
-                <p class="titulo">TED's</p>
-                <p class="subtitulo">Presentes Criativos</p>
+        <><div class="container">
+            <nav class="MenuLateral">
+                <div class="login-coluna-esquerda">
+                    <p class="titulo">TED's</p>
+                    <p class="subtitulo">Presentes Criativos</p>
+                </div>
+                <di class="MenuLinks">
+                    <a href="">CLIENTES</a>
+                    <a href="">PRODUTOS</a>
+                    <a href="">PEDIDOS</a>
+                    <a href="">VENDEDORES</a>
+                </di>
+            </nav>
+            <div class="workspace">
+                <div class="workspace-buttons">
+                    <button>+Cadastrar <br/> Novo Cliente</button>
+                </div>
             </div>
-            <di class="MenuLinks"> 
-                <a href="">CLIENTES</a>
-                <a href="">PRODUTOS</a>
-                <a href="">PEDIDOS</a>
-                <a href="">VENDEDORES</a>
-            </di>
-        </nav>
+        </div>
+        </>
     );
 }
 export default MainPage;
